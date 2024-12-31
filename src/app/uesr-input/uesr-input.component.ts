43f7cc1bef072d539706ad9f8fd1aed2d71375dd
mohamed mohran <mohamed01098187312@gmail.com>
2024-12-31 02:13:09 +0200

    using service

--- src/app/uesr-input/uesr-input.component.ts
+++ src/app/uesr-input/uesr-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, output, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-uesr-input',
@@ -10,24 +11,27 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './uesr-input.component.css'
 })
 export class UesrInputComponent {
-  calculate = output<InvestmentInput>();
-  enteredInitialInvestment = signal( '0');
-  enteredAnnualInvestment = signal( '0');
-  enteredExpectedReturn = signal( '5');
-  enteredDuration = signal( '10');
 
-  onSubit() { 
-    this.calculate.emit({
-      initialInvestment:+this.enteredInitialInvestment(), 
-      duration:+this.enteredDuration(),
-      expectedReturn:+this.enteredExpectedReturn(),
-      annualInvestment:+this.enteredAnnualInvestment()
-    }); 
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
+  constructor(private investmentService: InvestmentService) { }
+
+  onSubit() {
+    this.investmentService.calculateInvestmentResults(
+      {
+        initialInvestment: +this.enteredInitialInvestment(),
+        duration: +this.enteredDuration(),
+        expectedReturn: +this.enteredExpectedReturn(),
+        annualInvestment: +this.enteredAnnualInvestment()
+      }
+    )
     this.enteredInitialInvestment.set('0')
     this.enteredAnnualInvestment.set('0')
     this.enteredExpectedReturn.set('5')
     this.enteredDuration.set('10')
-    
+
   }
 }
